9e69c5ffd37c204b2fdbb155a33e03c3fcc53872
zsh <2905154639@qq.com>
2025-04-27 23:20:14 +0800

    "hello"

---
+++ hello_expo/app/index.tsx
@@ -0,0 +1,15 @@
+import 'react-native-reanimated';
+
+import { Text } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+    </>
+  );
+}
+// npx expo install expo@latest
